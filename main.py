8040f65c67b089aa1898ce247e1f43d271267c7f
Tellur <tellur1337@mail.ru>
2022-05-04 00:50:30 +0500

    task done

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+#First task- Author - Bolnykh Sofia
+s = input("Введите список строк через tab: ").split('\t')
+nl = input("Введите новую строку: ")
+def list(s):
+    count = 1
+    for x in s:
+        print(count, x)
+        count += 1
+n = len(s)
+i = n//2
+if i%2==1:
+    i=-1
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
+s.insert(i, nl)
+
+print(list(s))
+
+
 
 
