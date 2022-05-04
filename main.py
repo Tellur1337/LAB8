b56932f1bef305b2318f07d7dd67eaba7a906f70
Tellur <tellur1337@mail.ru>
2022-05-04 22:34:12 +0500

    +#SCV8

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-#First task- Author - Bolnykh Sofia
+#Second task- Author - Repin Nikita
 s = input("Введите список строк через tab: ").split('\t')
+num = int(input("Введите позицию вашей новой строки: "))
 nl = input("Введите новую строку: ")
 def list(s):
     count = 1
     for x in s:
         print(count, x)
         count += 1
-n = len(s)
-i = n//2
-if i%2==1:
-    i=-1
 
-
-s.insert(i, nl)
+s.clear(2)
+s.insert(num, nl)
 
 print(list(s))
-
 
 
 
